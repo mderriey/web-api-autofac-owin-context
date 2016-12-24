ddbaade89f4321ad39da94aa79865f1e7673969b
Mickaël Derriey <mderriey@gmail.com>
2016-12-24 21:38:06 +1100

    add VS solution

---
+++ WebApi.AutofacOwinContext/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using System.Web.Http;
+
+namespace WebApi.AutofacOwinContext
+{
+    [RoutePrefix("home")]
+    public class HomeController : ApiController
+    {
+        private readonly IOwinContext _context;
+
+        public HomeController(IOwinContext context)
+        {
+            _context = context;
+        }
+
+        [Route("")]
+        public string Get()
+        {
+            return _context.Request.Uri.ToString();
+        }
+    }
+}
---
+++ WebApi.AutofacOwinContext/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using Owin;
+using System.Web.Http;
+
+namespace WebApi.AutofacOwinContext
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(typeof(Startup).Assembly);
+            var container = builder.Build();
+
+            var httpConfiguration = new HttpConfiguration();
+            httpConfiguration.MapHttpAttributeRoutes();
+            httpConfiguration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacWebApi(httpConfiguration);
+            app.UseWebApi(httpConfiguration);
+        }
+    }
+}
